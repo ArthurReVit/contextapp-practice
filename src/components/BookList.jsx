2d6a0db77b8ab809f21d2969ea7dada94fc54165
ArthurRevit <arturo@descifra.mx>
2021-06-30 10:32:57 -0500

    Context practice.
    Signed-off-by: ArthurRevit <arturo@descifra.mx>

---
+++ src/components/BookList.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { BookContext } from "../context/BookContext";
+import { ThemeContext } from "../context/ThemeContext";
+
+const Booklist = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
+  const theme = isLightTheme ? light : dark;
+
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        {books.map((book) => {
+          return (
+            <li style={{ background: theme.ui }} key={book.id}>
+              {book.title}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Booklist;
